dc378e34862249e49f6aa9478bd72b1bf5ed5c5d
Iván <ijpulidos@unal.edu.co>
2020-07-06 00:43:57 -0500

    Adding setup.py file to install with pip.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='statlearn',
+    packages=['error'],
+    version='0.1.0',
+    description='Just for fun micro library for Statistical Learning.',
+    author='Ivan Pulido',
+    license='MIT',
+)
